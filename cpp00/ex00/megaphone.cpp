bc4aeaf35a7f45545c0e7727f9aa380884bc700c
michang <minseok128128@gmail.com>
2024-03-01 22:44:49 +0900

    Update 00-00 : argv[0] 출력 문제 해결

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,13 +6,13 @@
 /*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 22:20:12 by michang           #+#    #+#             */
-/*   Updated: 2024/03/01 22:38:34 by michang          ###   ########.fr       */
+/*   Updated: 2024/03/01 22:42:39 by michang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-char	to_uppercase(char c)
+char	get_uppercase(char c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (c - 'a' + 'A');
@@ -22,15 +22,10 @@ char	to_uppercase(char c)
 int	main(int argc, char **argv)
 {
 	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	else
-	{
-		for (int i = 0; argv[i]; i++)
-		{
-			for (int j = 0; argv[i][j]; j++)
-				std::cout << to_uppercase(argv[i][j]);
-		}
-		std::cout << std::endl;
-	}
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	for (int i = 1; argv[i]; i++)
+		for (int j = 0; argv[i][j]; j++)
+			std::cout << get_uppercase(argv[i][j]);
+	std::cout << std::endl;
 	return (0);
 }
