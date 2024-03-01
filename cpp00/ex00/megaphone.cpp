7e49773364fe3e753154eeadd9538ff17052dc71
michang <minseok128128@gmail.com>
2024-03-01 22:39:53 +0900

    Update 00-00 : megaphon.cpp 구현 완료

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,13 +6,31 @@
 /*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 22:20:12 by michang           #+#    #+#             */
-/*   Updated: 2024/03/01 22:26:33 by michang          ###   ########.fr       */
+/*   Updated: 2024/03/01 22:38:34 by michang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int	main(void)
+char	to_uppercase(char c)
 {
-	std::cout << "hello c++!\n";
+	if (c >= 'a' && c <= 'z')
+		return (c - 'a' + 'A');
+	return (c);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 0; argv[i]; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+				std::cout << to_uppercase(argv[i][j]);
+		}
+		std::cout << std::endl;
+	}
+	return (0);
 }
