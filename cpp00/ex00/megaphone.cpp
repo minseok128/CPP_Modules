2b72199ed06bba097e844801460d8098993311ff
michang <minseok128128@gmail.com>
2024-03-04 15:09:07 +0900

    Update 00-00 : toupper 사용

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,18 +6,11 @@
 /*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 22:20:12 by michang           #+#    #+#             */
-/*   Updated: 2024/03/01 22:42:39 by michang          ###   ########.fr       */
+/*   Updated: 2024/03/04 15:08:50 by michang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-
-char	get_uppercase(char c)
-{
-	if (c >= 'a' && c <= 'z')
-		return (c - 'a' + 'A');
-	return (c);
-}
 
 int	main(int argc, char **argv)
 {
@@ -25,7 +18,7 @@ int	main(int argc, char **argv)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	for (int i = 1; argv[i]; i++)
 		for (int j = 0; argv[i][j]; j++)
-			std::cout << get_uppercase(argv[i][j]);
+			std::cout << std::toupper(argv[i][j]);
 	std::cout << std::endl;
 	return (0);
 }
