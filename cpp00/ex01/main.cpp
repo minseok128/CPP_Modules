88fa4c85c3554d78faddf25c7bc879ae6e6113b6
michang <minseok128128@gmail.com>
2024-03-04 15:07:24 +0900

    Update 00-01 : main에 함수 껍대기 구현

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -6,11 +6,41 @@
 /*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 22:46:11 by michang           #+#    #+#             */
-/*   Updated: 2024/03/01 22:46:18 by michang          ###   ########.fr       */
+/*   Updated: 2024/03/04 15:06:46 by michang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(void)
+#include <iostream>
+#include <string>
+#include "PhoneBook.hpp"
+#include "Contact.hpp"
+
+void	search(PhoneBook& pb)
 {
 	
+}
+
+void	add(PhoneBook& pb)
+{
+	
+}
+
+int	main()
+{
+	std::string	input;
+	PhoneBook	pb;
+
+	while (42)
+	{
+		std::getline(std::cin, input);
+		if (input == "ADD")
+			add(pb);
+		else if (input == "SEARCH")
+			search(pb);
+		else if (input == "EXIT")
+			break;
+		else
+			std::cout << "Invalid command : " << input << std::endl;
+	}
+	return (0);
 }
