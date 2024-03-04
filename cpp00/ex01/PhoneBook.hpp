dc8be23bf16f36e595a021c878d53860c0b80bc1
michang <minseok128128@gmail.com>
2024-03-04 13:21:48 +0900

    Update 00-01 : PhoneBook class에 필요한 기본 적인 변수,함수 명세

--- cpp00/ex01/PhoneBook.hpp
+++ cpp00/ex01/PhoneBook.hpp
@@ -6,11 +6,26 @@
 /*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 22:47:08 by michang           #+#    #+#             */
-/*   Updated: 2024/03/01 22:52:48 by michang          ###   ########.fr       */
+/*   Updated: 2024/03/04 13:17:08 by michang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
+# include <string>
+# include "Contact.hpp"
+
+class PhoneBook
+{
+public :
+	PhoneBook();
+	~PhoneBook();
+	void	addContact();
+	Contact	&searchContact(int index);
+
+private :
+	Contact	_list[10];
+	int		_next;
+};
 
 #endif
