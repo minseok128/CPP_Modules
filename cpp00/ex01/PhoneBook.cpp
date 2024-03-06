2a9f0689ff349cccddbd768455862b67fee0171d
michang <minseok128128@gmail.com>
2024-03-06 22:32:42 +0900

    Update 00-01 : PhoneBook에 this 키워드 삭제

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 22:47:13 by michang           #+#    #+#             */
-/*   Updated: 2024/03/06 21:25:58 by michang          ###   ########.fr       */
+/*   Updated: 2024/03/06 21:45:45 by michang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,25 +24,25 @@ PhoneBook::~PhoneBook()
 
 int	PhoneBook::getIdx() const
 {
-	return (this->_idx);
+	return (_idx);
 }
 
 void	PhoneBook::addContact(std::string firstName, std::string lastName, \
 	std::string nickname, std::string phoneNumber, std::string secret)
 {
-	this->_list[_idx].setFirstName(firstName);
-	this->_list[_idx].setLastName(lastName);
-	this->_list[_idx].setNickname(nickname);
-	this->_list[_idx].setPhoneNumber(phoneNumber);
-	this->_list[_idx].setSecret(secret);
+	_list[_idx].setFirstName(firstName);
+	_list[_idx].setLastName(lastName);
+	_list[_idx].setNickname(nickname);
+	_list[_idx].setPhoneNumber(phoneNumber);
+	_list[_idx].setSecret(secret);
 	_idx = (_idx + 1) % MAXIDX;
 }
 
 Contact& PhoneBook::searchContact(long long idx)
 {
-	if (idx < 0 || idx >= MAXIDX || this->_list[idx].getFirstName().empty())
+	if (idx < 0 || idx >= MAXIDX || _list[idx].getFirstName().empty())
 		throw std::logic_error("index out of range");
-	return (this->_list[idx]);
+	return (_list[idx]);
 }
 
 void	PhoneBook::printAllContact()
@@ -50,10 +50,10 @@ void	PhoneBook::printAllContact()
 	std::cout << "\n     index|first name| last name|  nickname" << std::endl;
 	for (int i = 0; i < MAXIDX; i++)
 	{
-		if (!this->_list[i].getFirstName().empty())
+		if (!_list[i].getFirstName().empty())
 		{
 			std::cout << "         " << i << "|";
-			this->_list[i].printShort();
+			_list[i].printShort();
 		}
 	}
 }
