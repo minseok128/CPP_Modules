a892b349a83e3963255a3885b0f342740f0b1330
minseok128 <minseok128128@gmail.com>
2024-03-07 13:46:17 +0900

    Update 00-02 : Account class의 정적 함수 구현

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -6,8 +6,40 @@
 /*   By: minseok128 <minseok128@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 12:49:44 by minseok128        #+#    #+#             */
-/*   Updated: 2024/03/07 12:50:00 by minseok128       ###   ########.fr       */
+/*   Updated: 2024/03/07 13:45:55 by minseok128       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include "Account.hpp"
+
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+int	Account::getNbAccounts()
+{
+	return (_nbAccounts);
+}
+
+int	Account::getTotalAmount()
+{
+	return (_totalAmount);
+}
+
+int	Account::getNbDeposits()
+{
+	return (_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals()
+{
+	return (_totalNbWithdrawals);
+}
+
+void	Account::displayAccountsInfos()
+{
+	_displayTimestamp();
+	std::cout << "accounts:" << getNbAccounts() << ";total:" << getTotalAmount() << ";deposits:" << getNbDeposits() << ";withdrawals:" << getNbWithdrawals() << std::endl;
+}
