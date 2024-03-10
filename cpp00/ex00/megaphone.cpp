8464a1ed5743535b69d86a8450000e248dfaef2c
michang <minseok128128@gmail.com>
2024-03-10 17:41:39 +0900

    Update 00-00 : typo

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 22:20:12 by michang           #+#    #+#             */
-/*   Updated: 2024/03/10 17:40:40 by michang          ###   ########.fr       */
+/*   Updated: 2024/03/10 17:41:20 by michang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	main(int argc, char **argv)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	for (int i = 1; argv[i]; i++)
 		for (int j = 0; argv[i][j]; j++)
-			std::cout << static_cast<char>std::toupper(argv[i][j]);
+			std::cout << static_cast<char>(std::toupper(argv[i][j]));
 	std::cout << std::endl;
 	return (0);
 }
