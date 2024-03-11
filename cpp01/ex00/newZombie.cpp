8f08eeb4f361c2eeac629ac971b19e2a4b5fc4a0
michang <minseok128128@gmail.com>
2024-03-11 13:54:24 +0900

    Added 01-00 : newZombie 함수 구현

---
+++ cpp01/ex00/newZombie.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   newZombie.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/11 13:40:57 by michang           #+#    #+#             */
+/*   Updated: 2024/03/11 13:51:23 by michang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie	*newZombie(std::string name)
+{
+	Zombie	*zombie;
+
+	zombie = new Zombie();
+	zombie->setName(name);
+	return (zombie);
+}
