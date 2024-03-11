38f92755de1963043e422e580fed76a05a763752
michang <minseok128128@gmail.com>
2024-03-11 14:15:47 +0900

    Update 01-01 : main leaks 검사 수행 완료

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -6,18 +6,25 @@
 /*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 14:09:14 by michang           #+#    #+#             */
-/*   Updated: 2024/03/11 14:13:03 by michang          ###   ########.fr       */
+/*   Updated: 2024/03/11 14:15:03 by michang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Zombie.hpp"
 
+// #include <cstdio>
+// void	a()
+// {
+// 	system("leaks --list zombieHorde");
+// }
+
 int	main()
 {
 	int		n = 5;
 	Zombie	*horde;
 
+	// atexit(a);
 	horde = zombieHorde(n, "zombie");
 
 	for (int i = 0; i < n; ++i)
