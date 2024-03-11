0fa77dea6e049ee844cd93ddbf0fc71d2f72840a
michang <minseok128128@gmail.com>
2024-03-11 15:14:33 +0900

    Added 01-03 : main 문 구현

---
+++ cpp01/ex03/main.cpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/11 14:56:45 by michang           #+#    #+#             */
+/*   Updated: 2024/03/11 15:04:49 by michang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "HumanA.hpp"
+#include "HumanB.hpp"
+#include "Weapon.hpp"
+
+// #include <cstdio>
+// void	a()
+// {
+// 	system("leaks --list zombieHorde");
+// }
+
+int main()
+{
+	// atexit(a);
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	}
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanB jim("Jim");
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+	}
+}
