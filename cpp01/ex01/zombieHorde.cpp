c908cce2767aa37edba68b76f2d1aa1ad06ba14f
michang <minseok128128@gmail.com>
2024-03-11 14:13:47 +0900

    Update 01-01 : zombieHorde 함수 구현

--- cpp01/ex01/zombieHorde.cpp
+++ cpp01/ex01/zombieHorde.cpp
@@ -1,31 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*   zombieHorde.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/11 13:32:33 by michang           #+#    #+#             */
-/*   Updated: 2024/03/11 14:02:59 by michang          ###   ########.fr       */
+/*   Created: 2024/03/11 14:02:26 by michang           #+#    #+#             */
+/*   Updated: 2024/03/11 14:11:42 by michang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ZOMBIE_HPP
-# define ZOMBIE_HPP
-# include <string>
+#include "Zombie.hpp"
 
-class Zombie
+Zombie	*zombieHorde(int N, std::string name)
 {
-public:
-	Zombie();
-	~Zombie();
-	void	announce(void);
-	void	setName(std::string name);
+	Zombie	*horde;
 
-private:
-	std::string	_name;
-};
-
-Zombie	*zombieHorde(int N, std::string name);
-
-#endif
+	horde = new Zombie[N];
+	for (int i = 0; i < N; ++i)
+		horde[i].setName(name);
+	return (horde);
+}
