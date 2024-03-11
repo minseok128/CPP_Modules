14db82bebf9aac2fd970a9c402ca39b5a4fc7cda
michang <minseok128128@gmail.com>
2024-03-11 13:54:04 +0900

    Added 01-00 : Zombie class 구현

---
+++ cpp01/ex00/Zombie.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/11 13:37:15 by michang           #+#    #+#             */
+/*   Updated: 2024/03/11 13:40:35 by michang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+#include <iostream>
+
+Zombie::Zombie()
+	: _name("")
+{
+}
+
+Zombie::~Zombie()
+{
+	std::cout << this->_name << " is dead." << std::endl;
+}
+
+void	Zombie::announce(void)
+{
+	std::cout << _name << " : BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+void	Zombie::setName(std::string name)
+{
+	this->_name = name;
+}
