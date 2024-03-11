fd97c4d507800b46baea31ba3fa32fa3f3d0147a
michang <minseok128128@gmail.com>
2024-03-11 13:54:46 +0900

    Added 01-00 : randomChump 구현

---
+++ cpp01/ex00/randomChump.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   randomChump.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/11 13:45:06 by michang           #+#    #+#             */
+/*   Updated: 2024/03/11 13:46:12 by michang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+void	randomChump(std::string name)
+{
+	Zombie	zombie;
+
+	zombie.setName(name);
+	zombie.announce();
+	return ;
+}
