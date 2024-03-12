621ace8fa527b460a2cc1a17818d8fba83c3e942
minseok128 <minseok128128@gmail.com>
2024-03-12 15:53:43 +0900

    Added 02-00 : Fixed class 초기화

---
+++ cpp02/ex00/Fixed.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/12 15:42:49 by michang           #+#    #+#             */
+/*   Updated: 2024/03/12 15:53:21 by michang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+
+class Fixed
+{
+public:
+	Fixed(); // 기본 생성자
+	Fixed(const Fixed& src); // 복사 생성자
+	Fixed& operator=(const Fixed& src); // 복사 대입 연산자
+	~Fixed(); // 기본 소멸자
+
+private:
+	static const int	_FBITS;
+	int					_num;
+
+};
+
+#endif
