ddbddc53586e1159828dc8874c6d939af9fecc9e
minseok128 <minseok128128@gmail.com>
2024-03-12 16:09:00 +0900

    Update 02-00 : OCF 구현해봄

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -6,10 +6,11 @@
 /*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 15:45:43 by michang           #+#    #+#             */
-/*   Updated: 2024/03/12 15:57:52 by michang          ###   ########.fr       */
+/*   Updated: 2024/03/12 16:08:39 by michang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include "Fixed.hpp"
 
 const int Fixed::_FBITS = 8;
@@ -17,22 +18,26 @@ const int Fixed::_FBITS = 8;
 Fixed::Fixed()
 	: _num(0)
 {
+	std::cout << "Defalut Constructor" << std::endl;
 }
 
-Fixed::Fixed(const Fixed& src)
+Fixed::Fixed(const Fixed& rhs)
 {
-	
+	std::cout << "Copy Constructor" << std::endl;
+	*this = rhs;
+}
+
+Fixed&	Fixed::operator=(const Fixed& rhs)
+{
+	std::cout << "Copy Assignment Operator" << std::endl;
+	if (this != &rhs)
+	{
+		_num = rhs._num;
+	}
+	return (*this);
 }
 
 Fixed::~Fixed()
 {
-}
-
-Fixed&	Fixed::operator=(const Fixed& src)
-{
-	if (this != &src)
-	{
-		_num = src._num;
-	}
-	return (*this);
+	std::cout << "Default Destructor" << std::endl;
 }
