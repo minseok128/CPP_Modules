d1f15d3d32b93cddb22f48d3c36e6bf5e3856391
minseok128 <minseok128128@gmail.com>
2024-03-14 13:06:40 +0900

    Update 02-00 : typo

--- cpp02/ex00/main.cpp
+++ cpp02/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 16:40:25 by michang           #+#    #+#             */
-/*   Updated: 2024/03/12 16:50:31 by michang          ###   ########.fr       */
+/*   Updated: 2024/03/14 13:06:15 by michang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,10 @@
 
 int	main(void)
 {
-	Fixed a;
-	Fixed b(a);
-	Fixed c;
+	Fixed	a;
+	Fixed	b(a);
+	Fixed	c;
+
 	c = b;
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
