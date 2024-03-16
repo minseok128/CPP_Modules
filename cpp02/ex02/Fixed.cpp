b0e6a5bfb2c1c3ea613019656890c5b1632bcf81
michang <minseok128128@gmail.com>
2024-03-16 16:38:06 +0900

    Update 02-02 : 함수 호출 출력 모두 삭제

--- cpp02/ex02/Fixed.cpp
+++ cpp02/ex02/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 15:45:43 by michang           #+#    #+#             */
-/*   Updated: 2024/03/16 15:56:38 by michang          ###   ########.fr       */
+/*   Updated: 2024/03/16 16:37:47 by michang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,40 +18,29 @@ const int Fixed::_FBITS = 8;
 Fixed::Fixed()
 	: _rawBits(0)
 {
-	std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed(const Fixed& rhs)
 {
-	std::cout << "Copy constructor called" << std::endl;
 	*this = rhs;
 }
 
 Fixed::Fixed(const int src)
 	: _rawBits(src << _FBITS)
 {
-	std::cout << "Int constructor called" << std::endl;
 }
-
-// 0 10000100 01010011010111000010100
-// 0 10001100 01010011010111000010100
-// 0 10001100 01010011011000000000000
-// 101010 10011011
 
 Fixed::Fixed(const float src)
 	: _rawBits(roundf(src * (1 << _FBITS)))
 {
-	std::cout << "Float constructor called" << std::endl;
 }
 
 Fixed::~Fixed()
 {
-	std::cout << "Destructor called" << std::endl;
 }
 
 Fixed&	Fixed::operator=(const Fixed& rhs)
 {
-	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &rhs)
 		_rawBits = rhs._rawBits;
 	return (*this);
@@ -59,13 +48,11 @@ Fixed&	Fixed::operator=(const Fixed& rhs)
 
 int	Fixed::getRawBits(void) const
 {
-	std::cout << "getRawBits member function called" << std::endl;
 	return (_rawBits);
 }
 
 void	Fixed::setRawBits(int const raw)
 {
-	std::cout << "setRawBits member function called" << std::endl;
 	_rawBits = raw;
 }
 
