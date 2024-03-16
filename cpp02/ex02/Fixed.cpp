de1107b75a8d22fcb8e142e2dfcc4bf5269d0410
michang <minseok128128@gmail.com>
2024-03-16 18:01:37 +0900

    Update 02-02 : 간단한 연산자 구현

--- cpp02/ex02/Fixed.cpp
+++ cpp02/ex02/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 15:45:43 by michang           #+#    #+#             */
-/*   Updated: 2024/03/16 16:37:47 by michang          ###   ########.fr       */
+/*   Updated: 2024/03/16 17:45:51 by michang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,38 @@ Fixed&	Fixed::operator=(const Fixed& rhs)
 	return (*this);
 }
 
+Fixed	Fixed::operator+(const Fixed& r) const
+{
+	Fixed	tmp;
+
+	tmp._rawBits = _rawBits + r._rawBits;
+	return (tmp);
+}
+
+Fixed	Fixed::operator-(const Fixed& r) const
+{
+	Fixed	tmp;
+
+	tmp._rawBits = _rawBits - r._rawBits;
+	return (tmp);
+}
+
+Fixed	Fixed::operator*(const Fixed& r) const
+{
+	Fixed	tmp;
+
+	tmp._rawBits = (_rawBits * r._rawBits) >> _FBITS;
+	return (tmp);
+}
+
+Fixed	Fixed::operator/(const Fixed& r) const
+{
+	Fixed	tmp;
+
+	tmp._rawBits = (_rawBits / r._rawBits) >> _FBITS;
+	return (tmp);
+}
+
 int	Fixed::getRawBits(void) const
 {
 	return (_rawBits);
@@ -63,7 +95,7 @@ int	Fixed::toInt(void) const
 
 float	Fixed::toFloat(void) const
 {
-	return (_rawBits / ((1 << _FBITS) * 1.0));
+	return (static_cast<float>(_rawBits) / static_cast<float>((1 << _FBITS)));
 }
 
 std::ostream&	operator<<(std::ostream& os, const Fixed& obj)
--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -6,23 +6,40 @@
 /*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/16 16:20:18 by michang           #+#    #+#             */
-/*   Updated: 2024/03/16 16:21:44 by michang          ###   ########.fr       */
+/*   Updated: 2024/03/16 17:46:24 by michang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include "Fixed.hpp"
 
 int	main(void)
 {
-	Fixed		a;
-	Fixed const	b(Fixed(5.05f) * Fixed(2));
+	// Fixed		a;
+	// Fixed const	b(Fixed(5.05f) * Fixed(2));
 
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
-	std::cout << Fixed::max(a, b) << std::endl;
-	return 0;
+	// std::cout << a << std::endl;
+	// std::cout << ++a << std::endl;
+	// std::cout << a << std::endl;
+	// std::cout << a++ << std::endl;
+	// std::cout << a << std::endl;
+	// std::cout << b << std::endl;
+	// std::cout << Fixed::max(a, b) << std::endl;
+
+	std::cout << Fixed(0.75f) + Fixed(0.5f) << std::endl;
+	std::cout << Fixed(2000.75f) + Fixed(0.05f) << std::endl;
+	std::cout << std::endl;
+
+	std::cout << Fixed(1.125f) - Fixed(0.5f) << std::endl;
+	std::cout << Fixed(5.125f) - Fixed(10.5f) << std::endl;
+	std::cout << std::endl;
+
+	std::cout << Fixed(1.0f) * Fixed(0.5f) << std::endl;
+	std::cout << Fixed(1.125f) * Fixed(10.5f) << std::endl;
+	std::cout << std::endl;
+
+	std::cout << Fixed(255.0f) / Fixed(4) << std::endl;
+	std::cout << Fixed(1.125f) / Fixed(0.25f) << std::endl;
+	std::cout << std::endl;
+	return (0);
 }
