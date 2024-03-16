c8a9afad543bbd8c80a663d783b3726a3930ab22
michang <minseok128128@gmail.com>
2024-03-16 18:00:49 +0900

    Update 02-02 : const임을 명시

--- cpp02/ex02/Fixed.hpp
+++ cpp02/ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 15:42:49 by michang           #+#    #+#             */
-/*   Updated: 2024/03/16 16:35:26 by michang          ###   ########.fr       */
+/*   Updated: 2024/03/16 17:44:11 by michang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,17 +24,17 @@ public:
 	~Fixed();
 
 	Fixed&			operator=(const Fixed& rhs);
-	Fixed			operator+(const Fixed& r);
-	Fixed			operator-(const Fixed& r);
-	Fixed			operator*(const Fixed& r);
-	Fixed			operator/(const Fixed& r);
+	Fixed			operator+(const Fixed& r) const;
+	Fixed			operator-(const Fixed& r) const;
+	Fixed			operator*(const Fixed& r) const;
+	Fixed			operator/(const Fixed& r) const;
 
-	bool			operator==(const Fixed& r);
-	bool			operator!=(const Fixed& r);
-	bool			operator>(const Fixed& r);
-	bool			operator<(const Fixed& r);
-	bool			operator<=(const Fixed& r);
-	bool			operator>=(const Fixed& r);
+	bool			operator==(const Fixed& r) const;
+	bool			operator!=(const Fixed& r) const;
+	bool			operator>(const Fixed& r) const;
+	bool			operator<(const Fixed& r) const;
+	bool			operator<=(const Fixed& r) const;
+	bool			operator>=(const Fixed& r) const;
 
 	Fixed			operator++(int);
 	Fixed&			operator++();
