2e14e53b224993be4f130d8f0437a2f5306d1354
michang <minseok128128@gmail.com>
2024-03-18 14:10:50 +0900

    Update 02-03 : bsp구현

--- cpp02/ex03/bsp.cpp
+++ cpp02/ex03/bsp.cpp
@@ -6,7 +6,31 @@
 /*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 13:02:56 by michang           #+#    #+#             */
-/*   Updated: 2024/03/18 13:02:56 by michang          ###   ########.fr       */
+/*   Updated: 2024/03/18 14:04:12 by michang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Point.hpp"
+
+bool	bsp(const Point a, const Point b, const Point c, const Point point)
+{
+	const Point*	arr[4];
+
+	arr[0] = &a;
+	if (Point::cross_product(a, b, a, c) >= Fixed(0))
+	{
+		arr[1] = &b;
+		arr[2] = &c;
+	}
+	else
+	{
+		arr[1] = &c;
+		arr[2] = &b;
+	}
+	arr[3] = &a;
+	
+	for (int i = 0; i < 3; i++)
+		if (Point::cross_product(*arr[i], *arr[i + 1], *arr[i], point) < Fixed(0))
+			return (false);
+	return (true);
+}
