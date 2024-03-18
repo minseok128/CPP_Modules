183851cc3c71d0d5d268c4f680ad51eb3ee674d4
michang <minseok128128@gmail.com>
2024-03-18 12:59:22 +0900

    Update 02-02 : OCCF 절반 구현

--- cpp02/ex03/Point.cpp
+++ cpp02/ex03/Point.cpp
@@ -6,24 +6,38 @@
 /*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 12:18:35 by michang           #+#    #+#             */
-/*   Updated: 2024/03/18 12:35:10 by michang          ###   ########.fr       */
+/*   Updated: 2024/03/18 12:58:52 by michang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
 Point::Point()
+	: _x(), _y()
 {
 }
 
 Point::Point(const Point& obj)
 {
+	
 }
+
+Point::Point(float x, float y)
+	: _x(x), _y(y)
+{
+}
+
 
 Point::~Point()
 {
 }
 
-Point& Point::operator=(const Point& obj)
+Point&	Point::operator=(const Point& obj)
 {
+	if (this != &obj)
+	{
+		this->~Point();
+		new(this) Point(obj._x.toFloat(), obj._y.toFloat());
+	}
+	return (*this);
 }
--- cpp02/ex03/Point.hpp
+++ cpp02/ex03/Point.hpp
@@ -6,23 +6,26 @@
 /*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 12:18:28 by michang           #+#    #+#             */
-/*   Updated: 2024/03/18 12:35:06 by michang          ###   ########.fr       */
+/*   Updated: 2024/03/18 12:58:16 by michang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef POINT_HPP
 # define POINT_HPP
+# include "Fixed.hpp"
 
 class Point
 {
 public:
 	Point();
 	Point(const Point& obj);
+	Point(float x, float y);
 	~Point();
 	Point&	operator=(const Point& obj);
 
 private:
-
+	const Fixed	_x;
+	const Fixed	_y;
 };
 
 #endif
