4d93403342b00a8ba87a10f525c0016db251d3bd
minseok128 <minseok128128@gmail.com>
2024-03-19 11:31:59 +0900

    Added 03-00 : ClapTrap initialize

---
+++ cpp03/ex00/ClapTrap.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/19 11:31:21 by michang           #+#    #+#             */
+/*   Updated: 2024/03/19 11:31:23 by michang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap()
+{
+}
+
+ClapTrap::ClapTrap(const ClapTrap& obj)
+{
+}
+
+ClapTrap::~ClapTrap()
+{
+}
+
+ClapTrap&	ClapTrap::operator=(const ClapTrap& obj)
+{
+}
---
+++ cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/19 11:30:59 by michang           #+#    #+#             */
+/*   Updated: 2024/03/19 11:31:26 by michang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+class ClapTrap
+{
+public:
+	ClapTrap();
+	ClapTrap(const ClapTrap& obj);
+	~ClapTrap();
+	ClapTrap&	operator=(const ClapTrap& obj);
+
+private:
+
+};
+
+#endif
