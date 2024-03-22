cf9137eb2e6e294da677c0bb3598e3e9727178ce
minseok128 <minseok128128@gmail.com>
2024-03-22 16:22:44 +0900

    Update 03-01 : setter 삭제

--- cpp03/ex01/ClapTrap.hpp
+++ cpp03/ex01/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 11:30:59 by michang           #+#    #+#             */
-/*   Updated: 2024/03/19 15:58:40 by michang          ###   ########.fr       */
+/*   Updated: 2024/03/22 16:21:00 by michang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,9 @@ public:
 	void			takeDamage(unsigned int amount);
 	void			beRepaired(unsigned int amount);
 	std::string		getName() const;
-	void			setName(const std::string &name);
 	unsigned int	getHitPoints() const;
-	void			setHitPoints(unsigned int hitPoints);
 	unsigned int	getEnergyPoints() const;
-	void			setEnergyPoints(unsigned int energyPoints);
 	unsigned int	getAttackDamage() const;
-	void			setAttackDamage(unsigned int attackDamage);
 
 private:
 	std::string		_name;
