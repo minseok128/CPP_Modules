81667daedf6ebd4ed63e600b3bd254d647feded0
michang <minseok128128@gmail.com>
2024-03-30 16:36:58 +0900

    Update 03-03 : update main test code

--- cpp03/ex03/main.cpp
+++ cpp03/ex03/main.cpp
@@ -6,26 +6,28 @@
 /*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/30 15:04:24 by michang           #+#    #+#             */
-/*   Updated: 2024/03/30 15:05:39 by michang          ###   ########.fr       */
+/*   Updated: 2024/03/30 16:21:00 by michang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include "FragTrap.hpp"
+#include "DiamondTrap.hpp"
 
 int main()
 {
-	FragTrap A(std::string("A"));
-	FragTrap B(std::string("B"));
+	DiamondTrap A(std::string("A"));
+	DiamondTrap B(std::string("B"));
+	DiamondTrap C(A);
 	std::cout << std::endl;
 
 	A.attack(B.getName());
 	B.takeDamage(A.getAttackDamage());
 	A.printInfo();
 	B.printInfo();
-
 	A.highFivesGuys();
-	B.highFivesGuys();
+	B.guardGate();
+	A.whoAmI();
+	B.whoAmI();
 
 	return (0);
 }
