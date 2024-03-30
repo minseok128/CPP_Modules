a25e8e9946ee3e8c8eda683eef4e68a8eef53516
michang <minseok128128@gmail.com>
2024-03-30 13:37:04 +0900

    Update 03-01 : main 테스트 코드 구현

--- cpp03/ex01/main.cpp
+++ cpp03/ex01/main.cpp
@@ -6,42 +6,31 @@
 /*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 15:35:53 by michang           #+#    #+#             */
-/*   Updated: 2024/03/19 16:05:33 by michang          ###   ########.fr       */
+/*   Updated: 2024/03/30 13:35:22 by michang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
 
-int main() {
-	ClapTrap	claptrap1("CL4P-TP1");
-	ClapTrap	claptrap2("CL4P-TP2");
+#include "ScavTrap.hpp"
 
-	// 서로 공격 시나리오
-	std::cout << "\n=== Battle Begins ===" << std::endl;
-	claptrap1.attack(claptrap2.getName());
-	claptrap2.takeDamage(claptrap1.getAttackDamage());
-	claptrap2.attack(claptrap1.getName());
-	claptrap1.takeDamage(claptrap2.getAttackDamage());
+int main()
+{
+	ScavTrap A(std::string("A"));
+	ScavTrap B(std::string("B"));
+	std::cout << std::endl;
 
-	// 상태 회복 시나리오
-	std::cout << "\n=== Recovery Time ===" << std::endl;
-	claptrap1.beRepaired(5);
-	claptrap2.beRepaired(5);
+	A.attack(B.getName());
+	B.takeDamage(A.getAttackDamage());
+	A.printInfo();
+	B.printInfo();
 
-	// 에너지 소진 시나리오
-	std::cout << "\n=== Energy Drain ===" << std::endl;
-	for (int i = 0; i < 12; ++i)
-		claptrap1.attack(claptrap2.getName());
-
-	// 큰 피해 시나리오
-	std::cout << "\n=== Critical Damage ===" << std::endl;
-	claptrap2.takeDamage(20);
-	std::cout << claptrap2.getName() << " has " << claptrap2.getHitPoints() << " hit points." << std::endl;
-
-	// 수리 시도, 에너지 없음
-	std::cout << "\n=== Attempt Repair Without Energy ===" << std::endl;
-	claptrap1.beRepaired(10);
+	A.guardGate();
+	B.guardGate();
+	A.printInfo();
+	B.printInfo();
+	A.guardGate();
 
 	return (0);
 }
