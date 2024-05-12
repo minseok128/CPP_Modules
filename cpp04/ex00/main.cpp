2947a649a3d39cce2015d9b86b5c5b5f37f7bcf2
minseok128 <minseok128128@gmail.com>
2024-05-12 22:31:50 +0900

    Update 04-00 : main formmating

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -8,9 +8,9 @@ int main()
 {
 	{
 		std::cout << "Animal test\n";
-		const Animal* meta = new Animal();
-		const Animal* j = new Dog();
-		const Animal* i = new Cat();
+		const Animal*	meta = new Animal();
+		const Animal*	j = new Dog();
+		const Animal*	i = new Cat();
  
 		std::cout << j->getType() << " " << std::endl;
 		std::cout << i->getType() << " " << std::endl;
@@ -21,11 +21,11 @@ int main()
 		delete i;
 		delete j;
 	}
-		
+
 	{
 		std::cout << "\n\nWrongAnimal test\n";
-		const WrongAnimal* meta = new WrongAnimal();
-		const WrongAnimal* i = new WrongCat();
+		const WrongAnimal*	meta = new WrongAnimal();
+		const WrongAnimal*	i = new WrongCat();
 
 		std::cout << i->getType() << " " << std::endl;
 		i->makeSound(); //will output the cat sound!
@@ -33,6 +33,6 @@ int main()
 		delete meta;
 		delete i;
 	}
-		
+
 	return 0;
 }
