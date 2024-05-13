4f13582be4c4e12559fdc97da8ae2901abfe2aec
michang <minseok128128@gmail.com>
2024-05-13 14:28:49 +0900

    Update 04-01 : main 구현

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -1,37 +1,46 @@
-#include "Animal.hpp"
+#include <iostream>
 #include "Dog.hpp"
 #include "Cat.hpp"
-#include "WrongAnimal.hpp"
-#include "WrongCat.hpp"
+
+void	a()
+{
+	system("leaks a.out");
+}
 
 int main()
 {
+	atexit(a);
 	{
-		std::cout << "Animal test\n";
-		const Animal*	meta = new Animal();
-		const Animal*	j = new Dog();
-		const Animal*	i = new Cat();
- 
-		std::cout << j->getType() << " " << std::endl;
-		std::cout << i->getType() << " " << std::endl;
-		i->makeSound(); //will output the cat sound!
-		j->makeSound();
-		meta->makeSound();
-		delete meta;
+		std::cout << "## Basic Test" << std::endl;
+		const Animal* j = new Dog();
+		const Animal* i = new Cat();
+		delete j; //should not create a leak
 		delete i;
-		delete j;
 	}
 
 	{
-		std::cout << "\n\nWrongAnimal test\n";
-		const WrongAnimal*	meta = new WrongAnimal();
-		const WrongAnimal*	i = new WrongCat();
+		std::cout << "\n## Array Test" << std::endl;
+		Animal *animals[6];
 
-		std::cout << i->getType() << " " << std::endl;
-		i->makeSound(); //will output the wrongAnimal sound!
-		meta->makeSound();
-		delete meta;
-		delete i;
+		std::cout << "\n### DOG" << std::endl;
+		animals[0] = new Dog();
+		animals[1] = new Dog(*(dynamic_cast<Dog*>(animals[0])));
+		animals[2] = new Dog();
+		*(dynamic_cast<Dog*>(animals[2])) = *(dynamic_cast<Dog*>(animals[0]));
+		
+		std::cout << "\n### CAT" << std::endl;
+		animals[3] = new Cat();
+		animals[4] = new Cat(*(dynamic_cast<Cat*>(animals[3])));
+		animals[5] = new Cat();
+		*(dynamic_cast<Cat*>(animals[5])) = *(dynamic_cast<Cat*>(animals[3]));
+
+		std::cout << "\n### makeSound" << std::endl;
+		for (int i = 0; i < 6; i++)
+			animals[i]->makeSound();
+
+		std::cout << "\n### delete" << std::endl;
+		for (int i = 0; i < 6; i++)
+			delete animals[i];
 	}
 
 	return 0;
