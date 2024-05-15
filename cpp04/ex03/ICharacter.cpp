9011e686a73bf416cdf5942ef1498a3f275e3c46
michang <minseok128128@gmail.com>
2024-05-15 16:08:00 +0900

    Added 04-03 : ICharacter class init

---
+++ cpp04/ex03/ICharacter.cpp
@@ -0,0 +1,17 @@
+#include "ICharacter.hpp"
+
+ICharacter::ICharacter()
+{
+}
+
+ICharacter::ICharacter(const ICharacter& obj)
+{
+}
+
+ICharacter::~ICharacter()
+{
+}
+
+ICharacter&	ICharacter::operator=(const ICharacter& obj)
+{
+}
---
+++ cpp04/ex03/ICharacter.hpp
@@ -0,0 +1,16 @@
+#ifndef ICHARACTER_HPP
+# define ICHARACTER_HPP
+
+class ICharacter
+{
+public:
+	ICharacter();
+	ICharacter(const ICharacter& obj);
+	~ICharacter();
+	ICharacter&	operator=(const ICharacter& obj);
+
+private:
+
+};
+
+#endif
