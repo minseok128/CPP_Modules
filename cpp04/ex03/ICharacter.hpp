e0e5a73457f09867d86d3d77541c2a3bbffb7370
michang <minseok128128@gmail.com>
2024-05-15 17:27:47 +0900

    Update 04-03 : ICharacter formatting

--- cpp04/ex03/ICharacter.hpp
+++ cpp04/ex03/ICharacter.hpp
@@ -1,16 +1,16 @@
 #ifndef ICHARACTER_HPP
 # define ICHARACTER_HPP
+# include <string>
+# include "AMateria.hpp"
 
 class ICharacter
 {
 public:
-	ICharacter();
-	ICharacter(const ICharacter& obj);
-	~ICharacter();
-	ICharacter&	operator=(const ICharacter& obj);
-
-private:
-
+	virtual ~ICharacter() {}
+	virtual const std::string&	getName() const = 0;
+	virtual void				equip(AMateria* m) = 0;
+	virtual void				unequip(int idx) = 0;
+	virtual void				use(int idx, ICharacter& target) = 0;
 };
 
 #endif
