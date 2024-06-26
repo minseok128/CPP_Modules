cf900b717d85e862c0d7a4c37b56107fa62c2425
michang <minseok128128@gmail.com>
2024-06-26 21:16:01 +0900

    Added 05-00 : Bureaucrat class

---
+++ cpp05/ex00/Bureaucrat.hpp
@@ -0,0 +1,39 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+# include <iostream>
+# include <string>
+# include <exception>
+
+class Bureaucrat
+{
+public:
+	Bureaucrat();
+	Bureaucrat(std::string name, int grade);
+	Bureaucrat(const Bureaucrat& obj);
+	~Bureaucrat();
+	Bureaucrat&	operator=(const Bureaucrat& obj);
+	std::string	getName() const;
+	int			getGrade() const;
+	void		incrementGrade();
+	void		decrementGrade();
+
+private:
+	void				_setGrade(int amount);
+	const std::string	_name;
+	int					_grade;
+
+	class GradeTooHighException : public std::exception
+	{
+	public:
+		virtual const char*	what() const throw();
+	};
+	class GradeTooLowException : public std::exception
+	{
+	public:
+		virtual const char*	what() const throw();
+	};
+};
+
+std::ostream&	operator<<(std::ostream& os, const Bureaucrat& obj);
+
+#endif
