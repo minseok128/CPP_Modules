fd4d04f77cdec60d2d6343f11d00c47e0091c34f
michang <minseok128128@gmail.com>
2024-06-26 21:40:52 +0900

    Update 05-00 : formatting with MS style guide

--- cpp05/ex00/Bureaucrat.cpp
+++ cpp05/ex00/Bureaucrat.cpp
@@ -1,18 +1,15 @@
 #include "Bureaucrat.hpp"
 
-Bureaucrat::Bureaucrat()
-	: _name("Default"), _grade(1)
+Bureaucrat::Bureaucrat() : _name("Default"), _grade(1)
 {
 }
 
-Bureaucrat::Bureaucrat(std::string name, int grade)
-	: _name(name)
+Bureaucrat::Bureaucrat(std::string name, int grade) : _name(name)
 {
-	_setGrade(grade);
+    _setGrade(grade);
 }
 
-Bureaucrat::Bureaucrat(const Bureaucrat& obj)
-	: _name(obj._name), _grade(obj._grade)
+Bureaucrat::Bureaucrat(const Bureaucrat &obj) : _name(obj._name), _grade(obj._grade)
 {
 }
 
@@ -20,57 +17,57 @@ Bureaucrat::~Bureaucrat()
 {
 }
 
-Bureaucrat&	Bureaucrat::operator=(const Bureaucrat& obj)
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &obj)
 {
-	if (this != &obj)
-	{
-		this->~Bureaucrat();
-		new(this) Bureaucrat(obj);
-	}
-	return (*this);
+    if (this != &obj)
+    {
+        this->~Bureaucrat();
+        new (this) Bureaucrat(obj);
+    }
+    return (*this);
 }
 
 std::string Bureaucrat::getName() const
 {
-	return (_name);
+    return (_name);
 }
 
-int	Bureaucrat::getGrade() const
+int Bureaucrat::getGrade() const
 {
-	return (_grade);
+    return (_grade);
 }
 
-void	Bureaucrat::incrementGrade()
+void Bureaucrat::incrementGrade()
 {
-	_setGrade(_grade - 1);
+    _setGrade(_grade - 1);
 }
 
-void	Bureaucrat::decrementGrade()
+void Bureaucrat::decrementGrade()
 {
-	_setGrade(_grade + 1);
+    _setGrade(_grade + 1);
 }
 
-void	Bureaucrat::_setGrade(int grade)
+void Bureaucrat::_setGrade(int grade)
 {
-	if (grade < 1)
-		throw (Bureaucrat::GradeTooHighException());
-	if (grade > 150)
-		throw (Bureaucrat::GradeTooLowException());
-	_grade = grade;
+    if (grade < 1)
+        throw(Bureaucrat::GradeTooHighException());
+    if (grade > 150)
+        throw(Bureaucrat::GradeTooLowException());
+    _grade = grade;
 }
 
-const char*	Bureaucrat::GradeTooHighException::what() const throw()
+const char *Bureaucrat::GradeTooHighException::what() const throw()
 {
-	return ("grade is greater than the maximum value");
+    return ("grade is greater than the maximum value");
 }
 
-const char*	Bureaucrat::GradeTooLowException::what() const throw()
+const char *Bureaucrat::GradeTooLowException::what() const throw()
 {
-	return ("grade is less than the minimum value");
+    return ("grade is less than the minimum value");
 }
 
-std::ostream&	operator<<(std::ostream& os, const Bureaucrat& obj)
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &obj)
 {
-	os << obj.getName() << ", bureaucrat grade " << obj.getGrade();
-	return (os);	
+    os << obj.getName() << ", bureaucrat grade " << obj.getGrade();
+    return (os);
 }
--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -6,149 +6,144 @@
 /*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 20:23:49 by michang           #+#    #+#             */
-/*   Updated: 2024/06/26 21:19:09 by michang          ###   ########.fr       */
+/*   Updated: 2024/06/26 21:39:55 by michang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
 #include "Bureaucrat.hpp"
 
 int main()
 {
-	{
-		std::cout << "\n<INCREMENT GRADE TEST>\n";
-		try
-		{
-			std::cout << "Create Bureaucrat 'A' grade '0'\n";
-			Bureaucrat A("A", 0);
-			std::cout << A << std::endl;
-			A.incrementGrade();
-			std::cout << A << std::endl;
-		}
-		catch(const std::exception& e)
-		{
-			std::cerr << e.what() << '\n';
-		}
+    std::cout << "\n<INCREMENT GRADE TEST>\n";
+    try
+    {
+        std::cout << "Create Bureaucrat 'A' grade '0'\n";
+        Bureaucrat A("A", 0);
+        std::cout << A << std::endl;
+        A.incrementGrade();
+        std::cout << A << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 
-		try
-		{
-			std::cout << "Create Bureaucrat 'B' grade '1'\n";
-			Bureaucrat B("B", 1);
-			std::cout << B << std::endl;
-			B.incrementGrade();
-			std::cout << B << std::endl;
-		}
-		catch(const std::exception& e)
-		{
-			std::cerr << e.what() << '\n';
-		}
+    try
+    {
+        std::cout << "Create Bureaucrat 'B' grade '1'\n";
+        Bureaucrat B("B", 1);
+        std::cout << B << std::endl;
+        B.incrementGrade();
+        std::cout << B << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 
-		try
-		{
-			std::cout << "Create Bureaucrat 'C' grade '75'\n";
-			Bureaucrat C("C", 75);
-			std::cout << C << std::endl;
-			C.incrementGrade();
-			std::cout << C << std::endl;
-		}
-		catch(const std::exception& e)
-		{
-			std::cerr << e.what() << '\n';
-		}
+    try
+    {
+        std::cout << "Create Bureaucrat 'C' grade '75'\n";
+        Bureaucrat C("C", 75);
+        std::cout << C << std::endl;
+        C.incrementGrade();
+        std::cout << C << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 
-		try
-		{
-			std::cout << "Create Bureaucrat 'D' grade '150'\n";
-			Bureaucrat D("D", 150);
-			std::cout << D << std::endl;
-			D.incrementGrade();
-			std::cout << D << std::endl;
-		}
-		catch(const std::exception& e)
-		{
-			std::cerr << e.what() << '\n';
-		}
+    try
+    {
+        std::cout << "Create Bureaucrat 'D' grade '150'\n";
+        Bureaucrat D("D", 150);
+        std::cout << D << std::endl;
+        D.incrementGrade();
+        std::cout << D << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 
-		try
-		{
-			std::cout << "Create Bureaucrat 'E' grade '151'\n";
-			Bureaucrat E("E", 151);
-			std::cout << E << std::endl;
-			E.incrementGrade();
-			std::cout << E << std::endl;
-		}
-		catch(const std::exception& e)
-		{
-			std::cerr << e.what() << '\n';
-		}
-	}
+    try
+    {
+        std::cout << "Create Bureaucrat 'E' grade '151'\n";
+        Bureaucrat E("E", 151);
+        std::cout << E << std::endl;
+        E.incrementGrade();
+        std::cout << E << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 
-	{
-		std::cout << "\n\n<DECREMENT GRADE TEST>\n";
-		try
-		{
-			std::cout << "Create Bureaucrat 'A' grade '0'\n";
-			Bureaucrat A("A", 0);
-			std::cout << A << std::endl;
-			A.decrementGrade();
-			std::cout << A << std::endl;
-		}
-		catch(const std::exception& e)
-		{
-			std::cerr << e.what() << '\n';
-		}
+    std::cout << "\n\n<DECREMENT GRADE TEST>\n";
+    try
+    {
+        std::cout << "Create Bureaucrat 'A' grade '0'\n";
+        Bureaucrat A("A", 0);
+        std::cout << A << std::endl;
+        A.decrementGrade();
+        std::cout << A << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 
-		try
-		{
-			std::cout << "Create Bureaucrat 'B' grade '1'\n";
-			Bureaucrat B("B", 1);
-			std::cout << B << std::endl;
-			B.decrementGrade();
-			std::cout << B << std::endl;
-		}
-		catch(const std::exception& e)
-		{
-			std::cerr << e.what() << '\n';
-		}
+    try
+    {
+        std::cout << "Create Bureaucrat 'B' grade '1'\n";
+        Bureaucrat B("B", 1);
+        std::cout << B << std::endl;
+        B.decrementGrade();
+        std::cout << B << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 
-		try
-		{
-			std::cout << "Create Bureaucrat 'C' grade '75'\n";
-			Bureaucrat C("C", 75);
-			std::cout << C << std::endl;
-			C.decrementGrade();
-			std::cout << C << std::endl;
-		}
-		catch(const std::exception& e)
-		{
-			std::cerr << e.what() << '\n';
-		}
+    try
+    {
+        std::cout << "Create Bureaucrat 'C' grade '75'\n";
+        Bureaucrat C("C", 75);
+        std::cout << C << std::endl;
+        C.decrementGrade();
+        std::cout << C << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 
-		try
-		{
-			std::cout << "Create Bureaucrat 'D' grade '150'\n";
-			Bureaucrat D("D", 150);
-			std::cout << D << std::endl;
-			D.decrementGrade();
-			std::cout << D << std::endl;
-		}
-		catch(const std::exception& e)
-		{
-			std::cerr << e.what() << '\n';
-		}
+    try
+    {
+        std::cout << "Create Bureaucrat 'D' grade '150'\n";
+        Bureaucrat D("D", 150);
+        std::cout << D << std::endl;
+        D.decrementGrade();
+        std::cout << D << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 
-		try
-		{
-			std::cout << "Create Bureaucrat 'E' grade '151'\n";
-			Bureaucrat E("E", 151);
-			std::cout << E << std::endl;
-			E.decrementGrade();
-			std::cout << E << std::endl;
-		}
-		catch(const std::exception& e)
-		{
-			std::cerr << e.what() << '\n';
-		}
-	}
-	return (0);
+    try
+    {
+        std::cout << "Create Bureaucrat 'E' grade '151'\n";
+        Bureaucrat E("E", 151);
+        std::cout << E << std::endl;
+        E.decrementGrade();
+        std::cout << E << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    return (0);
 }
