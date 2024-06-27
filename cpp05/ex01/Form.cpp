793182b95a0a3ed6069cb046a41a04bc71f117f2
minseok128 <minseok128128@gmail.com>
2024-06-27 23:46:41 +0900

    Update 05-01 : * formatting with google

--- cpp05/ex01/Form.cpp
+++ cpp05/ex01/Form.cpp
@@ -1,35 +1,40 @@
 #include "Form.hpp"
 
 Form::Form()
-    : _name("default"), _signed(false), _gradeToSign(150),
-      _gradeToExecute(150) {}
+	: _name("default"),
+	  _signed(false),
+	  _gradeToSign(150),
+	  _gradeToExecute(150) {}
 
 Form::Form(std::string name, int gradeToSign, int gradeToExecute)
-    : _name(name), _signed(false), _gradeToSign(gradeToSign),
-      _gradeToExecute(gradeToExecute) {}
+	: _name(name),
+	  _signed(false),
+	  _gradeToSign(gradeToSign),
+	  _gradeToExecute(gradeToExecute) {}
 
-Form::Form(const Form &obj)
-    : _name(obj._name), _signed(obj._signed), _gradeToSign(obj._gradeToSign),
-      _gradeToExecute(obj._gradeToExecute) {}
+Form::Form(const Form& obj)
+	: _name(obj._name),
+	  _signed(obj._signed),
+	  _gradeToSign(obj._gradeToSign),
+	  _gradeToExecute(obj._gradeToExecute) {}
 
 Form::~Form() {}
 
-Form &Form::operator=(const Form &obj) {
-  if (this != &obj) {
-    this->~Form();
-    new (this) Form(obj);
-  }
-  return (*this);
+Form& Form::operator=(const Form& obj) {
+	if (this != &obj) {
+		this->~Form();
+		new (this) Form(obj);
+	}
+	return (*this);
 }
 
 std::string Form::getName() const { return (_name); }
 
-void Form::beSigned(const Bureaucrat &obj) {
-  if (obj.getGrade() > _gradeToSign)
-    throw Form::GradeTooLowException();
-  _signed = true;
+void Form::beSigned(const Bureaucrat& obj) {
+	if (obj.getGrade() > _gradeToSign) throw Form::GradeTooLowException();
+	_signed = true;
 }
 
-const char *Form::GradeTooHighException::what() const throw() {
-  return ("Form grade is too high");
+const char* Form::GradeTooHighException::what() const throw() {
+	return ("Form grade is too high");
 }
--- cpp05/ex01/Form.hpp
+++ cpp05/ex01/Form.hpp
@@ -3,37 +3,35 @@
 #include <exception>
 #include <iostream>
 #include <string>
+
 #include "Bureaucrat.hpp"
 
-class Form
-{
-public:
-    Form();
-    Form(std::string name, int gradeToSign, int gradeToExecute);
-    Form(const Form &obj);
-    ~Form();
-    Form &operator=(const Form &obj);
-    std::string getName() const;
-    void beSigned(const Bureaucrat &obj);
+class Form {
+   public:
+	Form();
+	Form(std::string name, int gradeToSign, int gradeToExecute);
+	Form(const Form& obj);
+	~Form();
+	Form& operator=(const Form& obj);
+	std::string getName() const;
+	void beSigned(const Bureaucrat& obj);
 
-private:
-    std::string _name;
-    bool _signed;
-    int _gradeToSign;
-    int _gradeToExecute;
+   private:
+	std::string _name;
+	bool _signed;
+	int _gradeToSign;
+	int _gradeToExecute;
 
-    class GradeTooHighException : public std::exception
-    {
-    private:
-        virtual const char *what() const throw();
-    };
-    class GradeTooLowException : public std::exception
-    {
-    private:
-        virtual const char *what() const throw();
-    };
+	class GradeTooHighException : public std::exception {
+	   private:
+		virtual const char* what() const throw();
+	};
+	class GradeTooLowException : public std::exception {
+	   private:
+		virtual const char* what() const throw();
+	};
 };
 
-std::ostream &operator<<(std::ostream &os, const Form &obj);
+std::ostream& operator<<(std::ostream& os, const Form& obj);
 
 #endif
