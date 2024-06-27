414cee50d710b83e9bc3d73c5a7b18dbacfeaac1
minseok128 <minseok128128@gmail.com>
2024-06-27 23:51:50 +0900

    Update 05-00 : * formatting with LLVM

--- cpp05/ex00/Bureaucrat.cpp
+++ cpp05/ex00/Bureaucrat.cpp
@@ -1,73 +1,49 @@
 #include "Bureaucrat.hpp"
 
-Bureaucrat::Bureaucrat() : _name("Default"), _grade(1)
-{
+Bureaucrat::Bureaucrat() : _name("Default"), _grade(1) {}
+
+Bureaucrat::Bureaucrat(std::string name, int grade) : _name(name) {
+	_setGrade(grade);
 }
 
-Bureaucrat::Bureaucrat(std::string name, int grade) : _name(name)
-{
-    _setGrade(grade);
+Bureaucrat::Bureaucrat(const Bureaucrat &obj)
+	: _name(obj._name), _grade(obj._grade) {}
+
+Bureaucrat::~Bureaucrat() {}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &obj) {
+	if (this != &obj) {
+		this->~Bureaucrat();
+		new (this) Bureaucrat(obj);
+	}
+	return (*this);
 }
 
-Bureaucrat::Bureaucrat(const Bureaucrat &obj) : _name(obj._name), _grade(obj._grade)
-{
+std::string Bureaucrat::getName() const { return (_name); }
+
+int Bureaucrat::getGrade() const { return (_grade); }
+
+void Bureaucrat::incrementGrade() { _setGrade(_grade - 1); }
+
+void Bureaucrat::decrementGrade() { _setGrade(_grade + 1); }
+
+void Bureaucrat::_setGrade(int grade) {
+	if (grade < 1)
+		throw(Bureaucrat::GradeTooHighException());
+	if (grade > 150)
+		throw(Bureaucrat::GradeTooLowException());
+	_grade = grade;
 }
 
-Bureaucrat::~Bureaucrat()
-{
+const char *Bureaucrat::GradeTooHighException::what() const throw() {
+	return ("grade is greater than the maximum value");
 }
 
-Bureaucrat &Bureaucrat::operator=(const Bureaucrat &obj)
-{
-    if (this != &obj)
-    {
-        this->~Bureaucrat();
-        new (this) Bureaucrat(obj);
-    }
-    return (*this);
+const char *Bureaucrat::GradeTooLowException::what() const throw() {
+	return ("grade is less than the minimum value");
 }
 
-std::string Bureaucrat::getName() const
-{
-    return (_name);
-}
-
-int Bureaucrat::getGrade() const
-{
-    return (_grade);
-}
-
-void Bureaucrat::incrementGrade()
-{
-    _setGrade(_grade - 1);
-}
-
-void Bureaucrat::decrementGrade()
-{
-    _setGrade(_grade + 1);
-}
-
-void Bureaucrat::_setGrade(int grade)
-{
-    if (grade < 1)
-        throw(Bureaucrat::GradeTooHighException());
-    if (grade > 150)
-        throw(Bureaucrat::GradeTooLowException());
-    _grade = grade;
-}
-
-const char *Bureaucrat::GradeTooHighException::what() const throw()
-{
-    return ("grade is greater than the maximum value");
-}
-
-const char *Bureaucrat::GradeTooLowException::what() const throw()
-{
-    return ("grade is less than the minimum value");
-}
-
-std::ostream &operator<<(std::ostream &os, const Bureaucrat &obj)
-{
-    os << obj.getName() << ", bureaucrat grade " << obj.getGrade();
-    return (os);
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &obj) {
+	os << obj.getName() << ", bureaucrat grade " << obj.getGrade();
+	return (os);
 }
