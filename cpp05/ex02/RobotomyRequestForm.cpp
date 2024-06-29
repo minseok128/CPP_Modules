1d564dec7b5e3522d97ced2ab08f0c92327ed6fd
michang <minseok128128@gmail.com>
2024-06-29 20:45:35 +0900

    Update 05-02 : RobotomyRequestForm 생성자

--- cpp05/ex02/RobotomyRequestForm.cpp
+++ cpp05/ex02/RobotomyRequestForm.cpp
@@ -6,24 +6,27 @@
 /*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 20:04:20 by michang           #+#    #+#             */
-/*   Updated: 2024/06/29 20:04:22 by michang          ###   ########.fr       */
+/*   Updated: 2024/06/29 20:44:53 by michang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RobotomyRequestForm.hpp"
 
-RobotomyRequestForm::RobotomyRequestForm()
-{
-}
+RobotomyRequestForm::RobotomyRequestForm() : AForm("default target", 72, 45) {}
+
+RobotomyRequestForm::RobotomyRequestForm(std::string target)
+	: AForm(target, 72, 45) {}
 
 RobotomyRequestForm::RobotomyRequestForm(const RobotomyRequestForm& obj)
-{
-}
+	: AForm(obj) {}
 
-RobotomyRequestForm::~RobotomyRequestForm()
-{
-}
+RobotomyRequestForm::~RobotomyRequestForm() {}
 
-RobotomyRequestForm&	RobotomyRequestForm::operator=(const RobotomyRequestForm& obj)
-{
+RobotomyRequestForm&
+RobotomyRequestForm::operator=(const RobotomyRequestForm& obj) {
+	if (this != &obj) {
+		this->~RobotomyRequestForm();
+		new (this) RobotomyRequestForm(obj);
+	}
+	return (*this);
 }
--- cpp05/ex02/RobotomyRequestForm.hpp
+++ cpp05/ex02/RobotomyRequestForm.hpp
@@ -6,23 +6,23 @@
 /*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 20:04:08 by michang           #+#    #+#             */
-/*   Updated: 2024/06/29 20:04:17 by michang          ###   ########.fr       */
+/*   Updated: 2024/06/29 20:43:09 by michang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ROBOTOMYREQUESTFORM_HPP
-# define ROBOTOMYREQUESTFORM_HPP
+#define ROBOTOMYREQUESTFORM_HPP
+#include "AForm.hpp"
 
-class RobotomyRequestForm
-{
-public:
+class RobotomyRequestForm : public AForm {
+  public:
 	RobotomyRequestForm();
+	RobotomyRequestForm(std::string target);
 	RobotomyRequestForm(const RobotomyRequestForm& obj);
 	~RobotomyRequestForm();
-	RobotomyRequestForm&	operator=(const RobotomyRequestForm& obj);
+	RobotomyRequestForm& operator=(const RobotomyRequestForm& obj);
 
-private:
-
+  private:
 };
 
 #endif
