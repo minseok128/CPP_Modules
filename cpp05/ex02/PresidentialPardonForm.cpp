42cbc6c22de9a29a2f137ce914cad9d37dbc1a72
michang <minseok128128@gmail.com>
2024-06-29 20:47:57 +0900

    Update 05-02 : PresidentialPardonForm 생성자

--- cpp05/ex02/PresidentialPardonForm.cpp
+++ cpp05/ex02/PresidentialPardonForm.cpp
@@ -6,24 +6,29 @@
 /*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 20:04:45 by michang           #+#    #+#             */
-/*   Updated: 2024/06/29 20:04:47 by michang          ###   ########.fr       */
+/*   Updated: 2024/06/29 20:47:29 by michang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PresidentialPardonForm.hpp"
 
 PresidentialPardonForm::PresidentialPardonForm()
-{
-}
+	: AForm("default target", 25, 5) {}
 
-PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm& obj)
-{
-}
+PresidentialPardonForm::PresidentialPardonForm(std::string target)
+	: AForm(target, 25, 5) {}
 
-PresidentialPardonForm::~PresidentialPardonForm()
-{
-}
+PresidentialPardonForm::PresidentialPardonForm(
+	const PresidentialPardonForm& obj)
+	: AForm(obj) {}
 
-PresidentialPardonForm&	PresidentialPardonForm::operator=(const PresidentialPardonForm& obj)
-{
+PresidentialPardonForm::~PresidentialPardonForm() {}
+
+PresidentialPardonForm&
+PresidentialPardonForm::operator=(const PresidentialPardonForm& obj) {
+	if (this != &obj) {
+		this->~PresidentialPardonForm();
+		new (this) PresidentialPardonForm(obj);
+	}
+	return (*this);
 }
--- cpp05/ex02/PresidentialPardonForm.hpp
+++ cpp05/ex02/PresidentialPardonForm.hpp
@@ -6,23 +6,23 @@
 /*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 20:04:32 by michang           #+#    #+#             */
-/*   Updated: 2024/06/29 20:04:42 by michang          ###   ########.fr       */
+/*   Updated: 2024/06/29 20:46:01 by michang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PRESIDENTIALPARDONFORM_HPP
-# define PRESIDENTIALPARDONFORM_HPP
+#define PRESIDENTIALPARDONFORM_HPP
+#include "AForm.hpp"
 
-class PresidentialPardonForm
-{
-public:
+class PresidentialPardonForm : public AForm {
+  public:
 	PresidentialPardonForm();
+	PresidentialPardonForm(std::string target);
 	PresidentialPardonForm(const PresidentialPardonForm& obj);
 	~PresidentialPardonForm();
-	PresidentialPardonForm&	operator=(const PresidentialPardonForm& obj);
+	PresidentialPardonForm& operator=(const PresidentialPardonForm& obj);
 
-private:
-
+  private:
 };
 
 #endif
