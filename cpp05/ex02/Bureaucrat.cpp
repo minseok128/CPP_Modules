d43d65793dc572c1fbdadda24b18da0d17ff2c5c
michang <minseok128128@gmail.com>
2024-06-29 20:06:31 +0900

    Added 05-02 : from prev

---
+++ cpp05/ex02/Bureaucrat.cpp
@@ -0,0 +1,59 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() : _name("Default"), _grade(1) {}
+
+Bureaucrat::Bureaucrat(std::string name, int grade) : _name(name) {
+	_setGrade(grade);
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& obj)
+	: _name(obj._name), _grade(obj._grade) {}
+
+Bureaucrat::~Bureaucrat() {}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& obj) {
+	if (this != &obj) {
+		this->~Bureaucrat();
+		new (this) Bureaucrat(obj);
+	}
+	return (*this);
+}
+
+std::string Bureaucrat::getName() const { return (_name); }
+
+int Bureaucrat::getGrade() const { return (_grade); }
+
+void Bureaucrat::incrementGrade() { _setGrade(_grade - 1); }
+
+void Bureaucrat::decrementGrade() { _setGrade(_grade + 1); }
+
+void Bureaucrat::_setGrade(int grade) {
+	if (grade < 1)
+		throw(Bureaucrat::GradeTooHighException());
+	if (grade > 150)
+		throw(Bureaucrat::GradeTooLowException());
+	_grade = grade;
+}
+
+const char* Bureaucrat::GradeTooHighException::what() const throw() {
+	return ("grade is greater than the maximum value");
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw() {
+	return ("grade is less than the minimum value");
+}
+
+void Bureaucrat::signForm(Form& obj) const {
+	try {
+		obj.beSigned(*this);
+		std::cout << _name << " signed " << obj.getName() << std::endl;
+	} catch (const std::exception& e) {
+		std::cout << _name << " couldn’t sign " << obj.getName() << " because "
+				  << e.what() << std::endl;
+	}
+}
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& obj) {
+	os << obj.getName() << ", bureaucrat grade " << obj.getGrade();
+	return (os);
+}
