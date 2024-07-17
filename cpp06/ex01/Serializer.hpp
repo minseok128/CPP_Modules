95566b19210eb7eb0261e0ed14aecf24dba5ccd6
MinSeok Chang <minseok128128@gmail.com>
2024-07-17 10:48:23 +0000

    Update 06-01 : uintptr_t 이슈 해결

--- cpp06/ex01/Serializer.hpp
+++ cpp06/ex01/Serializer.hpp
@@ -14,6 +14,7 @@
 #define SERIALIZER_HPP
 #include <iostream>
 #include <string>
+#define uintptr_t unsigned long
 
 struct Data {
 	std::string name;
--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include "Serializer.hpp"
-#include <iostream>
 
 int main() {
 	Data data;
