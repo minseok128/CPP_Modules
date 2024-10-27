608857cf7723043af8febc10e245dd0bda634bbc
michang <minseok128128@gmail.com>
2024-10-27 15:08:55 +0900

    Update 09-00 : main 작성

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -6,36 +6,51 @@
 /*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 16:09:54 by michang           #+#    #+#             */
-/*   Updated: 2024/10/26 20:11:02 by michang          ###   ########.fr       */
+/*   Updated: 2024/10/27 14:46:55 by michang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
-int inputError() {
-	std::cout << "Error: input." << std::endl;
+int pintError(const std::string& error) {
+	std::cout << "Error: " << error << std::endl;
 	return 1;
 }
 
 int main(int argc, char** argv) {
 	std::ifstream inputFile(argv[1]);
 	std::istringstream stream;
-	std::string line, word;
+	std::string line, left, delimiter, right;
 
 	if (argc != 2 && !inputFile.is_open()) {
-		return inputError();
+		return pintError("could not open file.");
 	} else {
 		std::getline(inputFile, line);
 		stream.str(line);
 		stream.clear();
-		if (!(stream >> word && word == "date" && stream >> word &&
-			  word == "|" && stream >> word && word == "value" &&
-			  !(stream >> word)))
-			return inputError();
+		if (!(stream >> left && left == "date" && stream >> delimiter &&
+			  delimiter == "|" && stream >> right && right == "value" &&
+			  !(stream >> right)))
+			return pintError("could not open file.");
 	}
 
 	BitcoinExchange& exchange = BitcoinExchange::getInstance();
 	exchange.initializeDatabase("data.csv");
+
+	while (std::getline(inputFile, line)) {
+		stream.str(line);
+		stream.clear();
+
+		if (!(stream >> left && stream >> delimiter && delimiter == "|" &&
+			  stream >> right && !(stream >> right))) {
+			pintError("bad input => " + line);
+			continue;
+		}
+
+		std::string error = exchange.calculatePrice(left, right);
+		if (!error.empty())
+			pintError(error);
+	}
 
 	return 0;
 }
