cf47da52c7f604d71eb65662a4e2256198409b62
michang <minseok128128@gmail.com>
2024-11-19 19:37:13 +0900

    Update 09-02 : List도 구현 완료

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 22:05:22 by michang           #+#    #+#             */
-/*   Updated: 2024/11/19 17:39:54 by michang          ###   ########.fr       */
+/*   Updated: 2024/11/19 19:34:39 by michang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,14 +43,18 @@ void PmergeMe::printAll(const std::string& s) {
 	std::list<t_data*>::iterator it_l = _l.begin();
 
 	while (it_v != _v.end() && it_l != _l.end()) {
-		// if ((*it_v)->value != (*it_l)->value)
-		// 	std::cerr << "Unmatched values: ";
+		if ((*it_v)->value != (*it_l)->value)
+			std::cerr << "Unmatched values: ";
 		std::cout << (*it_v)->value << " ";
 		it_v++;
 		it_l++;
 	}
 	std::cout << std::endl;
 }
+
+int PmergeMe::getVCoutn() { return _vCount; }
+
+int PmergeMe::getLCount() { return _lCount; }
 
 int PmergeMe::getJNum(int k) {
 	if (k < 0)
@@ -123,9 +127,109 @@ void PmergeMe::sortVector() {
 }
 
 // List
-void PmergeMe::sortList() {
-	unsigned int h = _v.size() / 2;
+std::list<t_data*>::iterator PmergeMe::getIteratorAtIndex(int index) {
+	std::list<t_data*>::iterator it = _l.begin();
+	std::advance(it, index);
+	return it;
+}
 
-	_lCount = 0;
-	(void)h;
+void PmergeMe::insertList(int i) {
+	int left_index = 0;
+	int right_index = i;
+	std::list<t_data*>::iterator left_it = _l.begin();
+	std::list<t_data*>::iterator right_it = getIteratorAtIndex(i);
+
+	t_data* value_to_insert = (*getIteratorAtIndex(i))->left;
+
+	while (left_index < right_index) {
+		int mid_index = (left_index + right_index) / 2;
+		std::list<t_data*>::iterator mid_it = left_it;
+		std::advance(mid_it, mid_index - left_index);
+
+		if ((*mid_it)->value < value_to_insert->value) {
+			left_it = mid_it;
+			++left_it;
+			left_index = mid_index + 1;
+		} else {
+			right_it = mid_it;
+			right_index = mid_index;
+		}
+		_lCount++;
+	}
+
+	_l.insert(left_it, value_to_insert);
+
+	std::list<t_data*>::iterator next_it = getIteratorAtIndex(i + 1);
+	if (next_it != _l.end() && (*next_it)->right) {
+		*next_it = (*next_it)->right;
+	} else if (next_it != _l.end()) {
+		_l.erase(next_it);
+	}
+}
+
+void PmergeMe::sortList() {
+	unsigned int s = _l.size(), h = s / 2;
+
+	if (h == 0)
+		return;
+
+	std::list<t_data*>::iterator front_it = _l.begin();
+	std::list<t_data*>::iterator back_it = _l.end();
+	--back_it;
+
+	for (unsigned int i = 0; i < h; ++i) {
+		t_data* front_data = *front_it;
+		t_data* back_data = *back_it;
+
+		t_data* new_data;
+		if (front_data->value > back_data->value)
+			new_data = new t_data(front_data->value, back_data, front_data);
+		else
+			new_data = new t_data(back_data->value, front_data, back_data);
+		_lCount++;
+
+		*front_it = new_data;
+
+		std::list<t_data*>::iterator temp_it = back_it;
+		if (back_it == front_it) {
+			++front_it;
+		}
+		if (back_it != _l.begin()) {
+			--back_it;
+		}
+		_l.erase(temp_it);
+
+		++front_it;
+	}
+
+	if (s % 2) {
+		t_data* oddData = _l.back();
+		_l.pop_back();
+		sortList();
+		_l.push_back(new t_data(oddData->value, oddData, 0));
+	} else {
+		sortList();
+	}
+
+	int j = 1, flag = 0;
+	int prevJNum = getJNum(0), nowJNum = getJNum(1), jNumGap;
+	int nowLevel = _l.front()->level;
+
+	while (!flag) {
+		jNumGap = nowJNum - prevJNum;
+		int k = prevJNum * 2 + jNumGap - 1;
+		if (k >= static_cast<int>(_l.size())) {
+			k = _l.size() - 1;
+			flag = 1;
+		}
+		for (int l = 0; l < jNumGap && k >= 0; k--) {
+			std::list<t_data*>::iterator it = getIteratorAtIndex(k);
+			if ((*it)->level == nowLevel) {
+				insertList(k++);
+				l++;
+			}
+		}
+		prevJNum = nowJNum;
+		nowJNum = getJNum(++j);
+	}
 }
