1a9625ed21ac18297bfece24cfdc334883579a51
michang <minseok128128@gmail.com>
2024-11-19 19:36:40 +0900

    Update 09-02 : main 시간 테스트 완료

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -6,11 +6,12 @@
 /*   By: michang <michang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 16:06:23 by michang           #+#    #+#             */
-/*   Updated: 2024/11/19 17:04:57 by michang          ###   ########.fr       */
+/*   Updated: 2024/11/19 19:31:43 by michang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
+#include <ctime>
 
 void buildOriginal(int argc, char** argv) {
 	PmergeMe& pm = PmergeMe::getInstance();
@@ -24,7 +25,6 @@ void buildOriginal(int argc, char** argv) {
 			throw std::exception();
 		pm.pushBack(num);
 	}
-	// pm.printAll("Befor: ");
 }
 
 int main(int argc, char** argv) {
@@ -38,9 +38,31 @@ int main(int argc, char** argv) {
 	}
 
 	PmergeMe& pm = PmergeMe::getInstance();
+	pm.printAll("Befor: ");
+
+	clock_t startVector = clock();
 	pm.sortVector();
-	pm.debugVector();
-	pm.printAll("");
+	clock_t endVector = clock();
+	double durationVector = static_cast<double>(endVector - startVector) /
+							CLOCKS_PER_SEC * 1000000;
+
+	clock_t startList = clock();
+	pm.sortList();
+	clock_t endList = clock();
+	double durationList = static_cast<double>(endList - startList) /
+						  CLOCKS_PER_SEC * 1000000;
+
+	pm.printAll("After: ");
+	std::cout << "Time to process a range of " << argc - 1
+			  << " elements with std::vector : " << durationVector << " us"
+			  << std::endl;
+	std::cout << "Time to process a range of " << argc - 1
+			  << " elements with std::list : " << durationList << " us"
+			  << std::endl;
+
+	if (pm.getVCoutn() != pm.getLCount())
+		std::cerr << "Error: Vector count and List count are different"
+				  << std::endl;
 
 	exit(0);
 }
